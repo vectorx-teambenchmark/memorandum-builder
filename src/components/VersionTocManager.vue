7b257790d7f5351abda2685b4d0fcc824ded75b1
Chris Gary <chris.gary@vectorx.com>
2023-12-20 04:26:45 -0500

    WIP

--- src/components/VersionTocManager.vue
+++ src/components/VersionTocManager.vue
@@ -18,7 +18,7 @@ const tocData = computed(()=>{
         return prev;
     },{});
     let completeSections = props.sections.map(item => {
-        item.expanded = false;
+        item.expanded = (item.Id === expandedTocItem.value) ? true: false;
         item.selected = (item.Id === selectedTocItem.value) ? true : false;
         if(Object.hasOwn(groupedContents,item.Id)){
             item.children = groupedContents[item.Id];
@@ -31,6 +31,7 @@ const tocData = computed(()=>{
     return completeSections;
 });
 const selectedTocItem = ref('');
+const expandedTocItem = ref('');
 </script>
 
 <template>
@@ -41,7 +42,7 @@ const selectedTocItem = ref('');
                 <li v-for="tocItem in tocData" v-bind:key="tocItem.Id" aria-level="1" v-bind:aria-selected="tocItem.selected" v-bind:aria-expanded="tocItem.expanded" role="treeitem">
                     <div class="slds-tree__item">
                         <button v-bind:class="{'slds-button':true, 'slds-button_icon':true, 'slds-m-right_x-small':true, 'slds-hidden':!tocItem.hasChildren }" 
-                            v-bind:aria-hidden="!tocItem.hasChildren" tabindex="-1" title="Expand Tree Branch">
+                            v-bind:aria-hidden="!tocItem.hasChildren" tabindex="-1" title="Expand Tree Branch" v-on:click="expandedTocItem = tocItem.Id">
                             <svg class="slds-button__icon slds-button__icon_small" v-bind:aria-hidden="!tocItem.hasChildren">
                                 <use xlink:href="/src/assets/icons/utility-sprite/svg/symbols.svg#chevronright"></use>
                             </svg>
@@ -53,14 +54,14 @@ const selectedTocItem = ref('');
                     <ul v-if="tocItem.hasChildren" role="group">
                         <li v-for="tocChildItem in tocItem.children" v-bind:key="tocChildItem.Id" aria-level="2" role="treeitem">
                             <div class="slds-tree__item">
-                                <button class="slds-button slds-button_icon slds-m-right_x-small" aria-hidden="true" tabindex="-1" title="Expand Tree Branch">
+                                <button class="slds-button slds-button_icon slds-m-right_x-small slds-hidden" aria-hidden="true" tabindex="-1" title="Expand Tree Branch">
                                     <svg class="slds-button__icon slds-button__icon_small" aria-hidden="true">
                                         <use xlink:href="/src/assets/icons/utility-sprite/svg/symbols.svg#chevronright"></use>
                                     </svg>
                                     <span class="slds-assistive-text">Expand Tree Item</span>
                                 </button>
                                 <span class="slds-has-flexi-truncate">
-                                    <span class="slds-tree__item-label slds-truncate" v-bind:title="tocChildItem.Name">{{ tocChildren.Name }}</span>
+                                    <span class="slds-tree__item-label slds-truncate" v-bind:title="tocChildItem.Name">{{ tocChildItem.Name }}</span>
                                 </span>
                             </div>
                         </li>
