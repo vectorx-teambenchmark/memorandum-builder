54c59b95eb023853de7c40cdb771037bce73eaba
Chris Gary <chris.gary@vectorx.com>
2024-01-10 10:16:50 -0500

    Completion of IM Preview Link Button function

--- src/components/ImLinkManager.vue
+++ src/components/ImLinkManager.vue
@@ -22,18 +22,36 @@ const authStore = useAuthStore();
 const isUrlValid = computed(()=>{
     return versionInfo.value?.ExternalUrlExpiration__c !== undefined && versionInfo.value?.ExternalUrlExpiration__c !== null;
 });
+const versionUrl = computed(()=>{
+    return `${authStore.apiUrl}/services/data/${import.meta.env.VITE_SALESFORCE_VERSION}/sobjects/MemorandumVersion__c/${props.versionId}/`;
+});
+const expirationFormDate = computed({
+    get(){
+        console.log('The ExternalUrlExpiration is in the format: %s',JSON.stringify(versionInfo.value.ExternalUrlExpiration__c));
+        return versionInfo.value.ExternalUrlExpiration__c;
+    },
+    set(val){
+        console.log('Value Passed in for ExternalUrlExpireation: %s',JSON.stringify(val));
+        versionInfo.value.ExternalUrlExpiration__c = val;
+    }
+});
 const expirationDisplayDate = computed(()=>{
     let dt = DateTime.fromISO(versionInfo.value?.ExternalUrlExpiration__c);
     return (dt.isValid) ? dt.toLocaleString():'';
 });
 const expirationDisplayUrl = computed(()=>{
     //get the right community
-    //let selectedCommunity = communitiesList.value.find(comm => comm.)
+    let retVal = '';
+    let selectedCommunity = communitiesList.value.find(comm => comm.name === 'Invite Confirmation');
+    retVal = (selectedCommunity !== undefined) ? selectedCommunity.siteUrl : '';
+    return `${retVal}/cim-preview?c__id=${versionInfo.value.ExternalId__c}`;
 })
 
 //reactive properties
 const versionInfo = ref({});
 const communitiesList = ref([]);
+const showUpdateMessage = ref(false);
+const updateMessage = ref('');
 
 //internal functions
 function handleCalloutException(e) {
@@ -45,6 +63,11 @@ function handleCalloutException(e) {
         default:
             console.log('There was an error: %s',JSON.stringify(e,null,"\t"));
     }
+}
+function resetMessage(){
+    setTimeout(()=>{
+        showUpdateMessage.value = false;
+    },3000);
 }
 async function obtainVersionInfo(versionIdIn) {
     try {
@@ -59,21 +82,6 @@ async function obtainVersionInfo(versionIdIn) {
         handleCalloutException(e);
     }
 }
-async function handlePreviewGenerationInfo(){
-    let dt = DateTime.now().plus({'months':1});
-    let versionUpdateData = { ExternalUrlExpiration__c: `${dt.toFormat('yyyy-MM-dd')}` };
-    let versionUpdateUrl = `${authStore.apiUrl}/services/data/${import.meta.env.VITE_SALESFORCE_VERSION}/sobjects/MemorandumVersion__c/${props.versionId}/`;
-    try {
-        await axios.patch(versionUpdateUrl,versionUpdateData,{
-            headers:{'authorization':`Bearer ${authStore.bearerToken}`},
-            responseType:'json'
-        });
-        obtainVersionInfo(props.versionId);
-    } catch(e) {
-        handleCalloutException(e);
-    }
-    console.log('Update Object: %s',JSON.stringify(versionUpdateData,null,"\t"));
-}
 async function getCommunities(){
     let communitiesUrl = `${authStore.apiUrl}/services/data/${import.meta.env.VITE_SALESFORCE_VERSION}/connect/communities/?status=Live`;
     try {
@@ -82,6 +90,36 @@ async function getCommunities(){
             responseType:'json'
         });
         communitiesList.value = communitiesResponse.data.communities;
+        console.log('Communities Found: %s',JSON.stringify(communitiesList.value,null,"\t"));
+    } catch(e) {
+        handleCalloutException(e);
+    }
+}
+async function handlePreviewGenerationInfo(){
+    let dt = DateTime.now().plus({'months':1});
+    let versionUpdateData = { ExternalUrlExpiration__c: `${dt.toFormat('yyyy-MM-dd')}` };
+    try {
+        await axios.patch(versionUrl.value,versionUpdateData,{
+            headers:{'authorization':`Bearer ${authStore.bearerToken}`},
+            responseType:'json'
+        });
+        obtainVersionInfo(props.versionId);
+    } catch(e) {
+        handleCalloutException(e);
+    }
+}
+async function handleExpirationUpdate(){
+    let versionUpdateData = { ExternalUrlExpiration__c: versionInfo.value.ExternalUrlExpiration__c };
+    updateMessage.value = '';
+    try {
+        await axios.patch(versionUrl.value,versionUpdateData,{
+            headers:{'authorization':`Bearer ${authStore.bearerToken}`},
+            responseType:'json'
+        });
+        updateMessage.value = 'The Expiration Date has been updated.';
+        showUpdateMessage.value = true;
+        resetMessage();
+        obtainVersionInfo(props.versionId);
     } catch(e) {
         handleCalloutException(e);
     }
@@ -107,9 +145,22 @@ onBeforeMount(() => {
             <div class="slds-form-element">
                 <label class="slds-form-element__label" for="txtExternalUrl">External URL</label>
                 <div class="slds-form-element__control">
-                    <input type="text" id="txtExternalUrl" class="slds-input"/>
+                    <input type="text" id="txtExternalUrl" disabled v-model="expirationDisplayUrl" class="slds-input"/>
                 </div>
             </div>
+            <p class="slds-text-longform">If you need to extend the expiration date of the URL, please provide the new date below and click 'Update'.</p>
+            <div class="slds-form-element">
+                <label class="slds-form-element__label" for="txtExpDate">External URL Expiration</label>
+                <div class="slds-form-element__control">
+                    <input type="date" class="slds-input" v-model="expirationFormDate" id="txtExpDate"/>
+                </div>
+            </div>
+            <div class="slds-var-m-vertical_small">
+                <button class="slds-button slds-button_brand" v-on:click="handleExpirationUpdate">Update</button>
+            </div>
+            <p v-if="showUpdateMessage">
+                {{  updateMessage }}
+            </p>
         </div>
     </div>
 </template>
